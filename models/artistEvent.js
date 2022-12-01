38066bf2ba884c24182b601958ef33912eb6670e
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-12-01 09:40:58 +0100

    artistEvent model jointure

--- models/artistEvent.js
+++ models/artistEvent.js
@@ -1,24 +1,16 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const artistEvent = sequelize.define("artistEvent", {
-    userId: {
+  const ArtistEvent = sequelize.define("ArtistEvent", {
+    eventId: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // artistId: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
-    // venueId: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
-    // eventId: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
+    artistId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 
-  return artistEvent;
+  return ArtistEvent;
 };
