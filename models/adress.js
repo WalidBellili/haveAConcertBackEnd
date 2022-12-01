0acf952f165bbf88f5d38cbf4aada59b4e421fec
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-12-01 09:55:51 +0100

    adress model

---
+++ models/adress.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Adress = sequelize.define("Adress", {
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    venueId: {
+      type: DataTypes.INTEGER,
+    },
+    streetNumber: {
+      type: DataTypes.STRING,
+    },
+    streetName: {
+      type: DataTypes.STRING,
+    },
+    zipCode: {
+      type: DataTypes.STRING,
+    },
+    region: {
+      type: DataTypes.STRING,
+    },
+    pays: {
+      type: DataTypes.STRING,
+    },
+    additionalInfo: {
+      type: DataTypes.STRING,
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+    },
+  });
+
+  return Adress;
+};
